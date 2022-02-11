3411ce61e551e51d573105d2174344baa3141ae9
RemiFabre <remifabre1800@gmail.com>
2022-02-11 18:51:05 +0100

    Working version with ros2_control handling the joint state broadcast. This was hell and I hate everything and everybody

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -1,7 +1,17 @@
 import launch
-from launch.substitutions import Command, LaunchConfiguration
 import launch_ros
 import os
+
+from launch import LaunchDescription
+from launch import event_handlers
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, RegisterEventHandler
+from launch.conditions import IfCondition
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
@@ -12,32 +22,74 @@ def generate_launch_description():
     default_rviz_config_path = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
     world_path = os.path.join(pkg_share, 'world/my_world.sdf')
 
-    robot_state_publisher_node = launch_ros.actions.Node(
+    # robot_description_content = Command(
+    #     ['xacro ', LaunchConfiguration('model')])
+
+    robot_description_content = Command(
+        [
+            PathJoinSubstitution([FindExecutable(name='xacro')]),
+            ' ',
+            PathJoinSubstitution(
+                [FindPackageShare('zuuu_description'),
+                 'src', 'description', 'zuuu_description.urdf.xacro']
+            ),
+        ]
+    )
+
+    robot_description = {"robot_description": robot_description_content}
+
+    zuuu_controller = PathJoinSubstitution(
+        [
+            FindPackageShare(
+                "zuuu_description"),
+            "config",
+            "zuuu_controllers.yaml",
+        ]
+    )
+
+    robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(
-            ['xacro ', LaunchConfiguration('model')])}]
+        parameters=[robot_description]
     )
-    joint_state_publisher_node = launch_ros.actions.Node(
-        package='joint_state_publisher',
-        executable='joint_state_publisher',
-        name='joint_state_publisher',
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+
+    controller_manager_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_description, zuuu_controller],
+        output={
+            "stdout": "screen",
+            "stderr": "screen",
+        },
     )
-    rviz_node = launch_ros.actions.Node(
+    joint_state_broadcaster_spawner = Node(
+        package='controller_manager',
+        executable='spawner.py',
+        arguments=['joint_state_broadcaster'],
+        output='screen',
+    )
+    rviz_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
         output='screen',
         arguments=['-d', LaunchConfiguration('rvizconfig')],
     )
-    spawn_entity = launch_ros.actions.Node(
+    # gazebo = IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(
+    #         [PathJoinSubstitution(
+    #             [launch_ros.substitutions.FindPackageShare('gazebo_ros'), 'launch', 'gazebo.launch.py'])]
+    #     ),
+    #     launch_arguments={'verbose': 'true'}.items(),
+    # )
+    spawn_entity = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=['-entity', 'zuuu', '-topic', 'robot_description'],
+        arguments=['-entity', 'zuuu', '-topic',
+                   'robot_description'],
         output='screen'
     )
-    robot_localization_node = launch_ros.actions.Node(
+    robot_localization_node = Node(
         package='robot_localization',
         executable='ekf_node',
         name='ekf_filter_node',
@@ -46,21 +98,27 @@ def generate_launch_description():
                     {'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
 
-    return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                             description='Flag to enable joint_state_publisher_gui'),
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                             description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-                                             description='Absolute path to rviz config file'),
-        launch.actions.DeclareLaunchArgument(name='use_sim_time', default_value='True',
-                                             description='Flag to enable use_sim_time'),
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'controllers_file',
+            default_value=['zuuu_controllers.yaml'],
+            description='YAML file with the controllers configuration.',
+        ),
+        DeclareLaunchArgument(name='gui', default_value='True',
+                              description='Flag to enable joint_state_publisher_gui'),
+        DeclareLaunchArgument(name='model', default_value=default_model_path,
+                              description='Absolute path to robot urdf file'),
+        DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+                              description='Absolute path to rviz config file'),
+        DeclareLaunchArgument(name='use_sim_time', default_value='True',
+                              description='Flag to enable use_sim_time'),
+
         launch.actions.ExecuteProcess(
             cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so', world_path], output='screen'),
-
-        joint_state_publisher_node,
-        robot_state_publisher_node,
         spawn_entity,
+        controller_manager_node,
+        joint_state_broadcaster_spawner,
+        robot_state_publisher_node,
         robot_localization_node,
         rviz_node
     ])
