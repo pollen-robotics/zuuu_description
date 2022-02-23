61ca3a9e5590ac33d90ad884201165dff10ae4a7
RemiFabre <remifabre1800@gmail.com>
2022-02-23 11:36:13 +0100

    Tuning the nav stack to avoid the wall bug

--- launch/navigation.launch.py
+++ launch/navigation.launch.py
@@ -11,7 +11,7 @@ MAP_NAME = os.environ['ZUUU_MAP_NAME']
 
 def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time', default='true')
-    map_dir = LaunchConfiguration(
+    map_path = LaunchConfiguration(
         'map',
         default=os.path.join(
             get_package_share_directory('zuuu_description'),
@@ -19,12 +19,20 @@ def generate_launch_description():
             MAP_NAME+'.yaml'))
 
     param_file_name = 'nav2_params.yaml'
-    param_dir = LaunchConfiguration(
+    param_path = LaunchConfiguration(
         'params_file',
         default=os.path.join(
             get_package_share_directory('zuuu_description'),
             'config/',
             param_file_name))
+
+    bt_file_name = 'zuuu_bt.xml'
+    bt_path = LaunchConfiguration(
+        'bt_file',
+        default=os.path.join(
+            get_package_share_directory('zuuu_description'),
+            'config/',
+            bt_file_name))
 
     nav2_launch_file_dir = os.path.join(
         get_package_share_directory('nav2_bringup'), 'launch')
@@ -32,25 +40,29 @@ def generate_launch_description():
     return LaunchDescription([
         DeclareLaunchArgument(
             'map',
-            default_value=map_dir,
+            default_value=map_path,
             description='Full path to map file to load'),
 
         DeclareLaunchArgument(
             'params_file',
-            default_value=param_dir,
+            default_value=param_path,
             description='Full path to param file to load'),
 
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='true',
             description='Use simulation (Gazebo) clock if true'),
+        DeclareLaunchArgument(
+            name='default_bt_xml_filename',
+            default_value=bt_path,
+            description='Full path to the behavior tree xml file to use'),
 
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource(
                 [nav2_launch_file_dir, '/bringup_launch.py']),
             launch_arguments={
-                'map': map_dir,
+                'map': map_path,
                 'use_sim_time': use_sim_time,
-                'params_file': param_dir}.items(),
+                'params_file': param_path}.items(),
         )
     ])
